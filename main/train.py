5b7d63493a42130eed29ae47e951f549f1e678f6
David <39951721+Sateus@users.noreply.github.com>
2023-07-12 11:31:27 +0200

    Added first program for weight measurement.

--- main/train.py
+++ main/train.py
@@ -107,7 +107,7 @@ def comp_train(
       batch_size: batch size.
 
     Returns:
-      Trained model and histroy.
+      Trained model and history.
     """
     history = model.fit(x_train, y_train,
                         batch_size=batch_size,
@@ -115,3 +115,90 @@ def comp_train(
                         verbose=2,
                         validation_data=(x_test, y_test))
     return model, history.history
+
+
+def weight_measure(
+        model,
+        layer_str,
+        epochs_train,
+        lr_schedule,
+        x_train,
+        y_train,
+        batch_size,
+        measure_epochs_only=False):
+    """
+    Trains a model and measure its weights.
+
+    Args:
+      model: model.
+      layer_str: layer str where to compute the weights
+      epochs_train: number of epochs to train.
+      x_train: training samples.
+      y_train: training labels.
+      batch_size: batch size.
+      measure_epochs_only: wether the weights are measured
+        only once per epoch oder on every batch defaults to False.
+
+    Returns:
+      Trained model and weights.
+    """
+    if layer_str[:6] == "layers":
+        layer_name = getattr(model, "layers")[int(
+            layer_str[7:(len(layer_str)-1)])]
+    else:
+        layer_name = getattr(model, layer_str)
+    layer_size = np.prod(layer_name.kernel.shape)
+    layer_dtype = np.float32
+
+    train_ds = tf.data.Dataset.from_tensor_slices(
+        (x_train, y_train)).shuffle(10000).batch(batch_size)
+
+    n_batches = train_ds.cardinality().numpy()
+
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule)
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+
+    if measure_epochs_only:
+        @tf.function
+        def train_step(images, labels):
+            with tf.GradientTape() as tape:
+                predictions = model(images, training=True)
+                loss = loss_object(labels, predictions)
+                loss += sum(model.losses)
+            gradients = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            train_loss(loss)
+        weights = np.zeros((epochs_train, layer_size), dtype=layer_dtype)
+        for epoch in range(epochs_train):
+            train_loss.reset_states()
+            print('epoch: '+str(epoch)+' of '+str(epochs_train))
+
+            for images, labels in train_ds:
+                train_step(images, labels)
+            templ = layer_name.get_weights()[0].flatten()
+            weights[epoch] = templ
+            print(train_loss.result())
+    else:
+        @tf.function
+        def train_step(images, labels):
+            with tf.GradientTape() as tape:
+                predictions = model(images, training=True)
+                loss = loss_object(labels, predictions)
+                loss += sum(model.losses)
+            gradients = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            train_loss(loss)
+        weights = np.zeros((epochs_train*n_batches, layer_size), dtype=layer_dtype)
+        index_1 = 0
+        for epoch in range(epochs_train):
+            train_loss.reset_states()
+            print('epoch: '+str(epoch)+' of '+str(epochs_train))
+
+            for images, labels in train_ds:
+                train_step(images, labels)
+                templ = layer_name.get_weights()[0].flatten()
+                weights[index_1] = templ
+                index_1 += 1
+            print(train_loss.result())
+    return model, weights
